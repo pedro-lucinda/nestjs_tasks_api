18f237632396b74d405851766ba35f161a00499d
Pedro Lucinda <pedrolucinda2021@gmail.com>
2021-09-23 05:23:02 -0300

    chore: removed unused imports

--- src/tasks/DTO/update-task-status.dto.ts
+++ src/tasks/DTO/update-task-status.dto.ts
@@ -1,4 +1,4 @@
-import { IsEnum, IsString, IsUUID } from 'class-validator';
+import { IsEnum } from 'class-validator';
 import { TasksStatus } from '../types/task-status.enum';
 
 export class UpdateTaskStatusDTO {
